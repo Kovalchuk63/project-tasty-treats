069389428eeab4d69cc668dafe33ecb723c9cea2
Anton240896 <antonalipov791@gmail.com>
2023-09-14 21:48:22 +0300

    add

--- src/js/list-of-popular.js
+++ src/js/list-of-popular.js
@@ -1,29 +1,44 @@
 const popularRecipe = document.querySelector('.js-popular-recipes');
 /*=================BACK-END===========================*/
-serviceRecype()
-    .then((data) => {
-        console.log(data);
-// popularRecipe.insertAdjacentHTML('beforeend', createMarkupCard(data))
-    })
-    .catch(err => console.log(err))
-    function serviceRecype() {
+// serviceRecype()
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch(err => console.log(err))
+
+//     function serviceRecype() {
         const URL =  'https://tasty-treats-backend.p.goit.global/api/recipes/popular';
-        return fetch(`${URL}`)
-    .then((resp) => {
-    if(!resp.ok) {
-        throw new Error('Error');
-}
-    return resp.json()
-});
+//         return fetch(`${URL}`)
+//     .then((resp) => {
+//         // popularRecipe.insertAdjacentHTML('beforeend', createMarkupCard(data))
+
+//    console.log(resp);
+//     return resp.json()
+// });
+//     }
+
+async function serviceRecype() {
+    try {
+        const resp = await fetch(URL);
+        const data = await resp.json()
+        console.log(data);
     }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+serviceRecype();
 /*=================SHOW-CARD===========================*/
 function createMarkupCard(arr) {
-return arr.map(({preview,title,description}) => {
+return arr.map(({preview, title, description}) => {
     `<li class ="popular-recipe-list">
-        <img class="img-dish" src=" 
-        "https://www.themealdb.com/images/media/meals/tqtywx1468317395.jpg"${preview}" alt="${title}">
+        <img class="img-dish" src="${preview}" 
+         alt="${title}">
         <h2 class= "name-dish">${title}</h2>
         <h3 class="deskription-dish">${description}</h3>
     </li>`
 }).join();
 }
+
+popularRecipe.insertAdjacentHTML('beforeend',)
