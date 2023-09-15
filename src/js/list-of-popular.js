2d8452b83591ab97e70f568ae37d7b1ec3b905f3
Anton240896 <antonalipov791@gmail.com>
2023-09-15 11:06:40 +0300

    add cards

--- src/js/list-of-popular.js
+++ src/js/list-of-popular.js
@@ -3,59 +3,31 @@ const popularRecipe = document.querySelector('.js-popular-recipes');
 serviceRecype()
     .then((data) => {
         console.log(data);
+        popularRecipe.insertAdjacentHTML('beforeend', createMarkupCard(data));
+
     })
     .catch(err => console.log(err))
 
-    function serviceRecype() {
+    async function serviceRecype() {
         const URL =  'https://tasty-treats-backend.p.goit.global/api/recipes/popular';
-        return fetch(`${URL}`)
-    .then((resp) => {
-        // popularRecipe.insertAdjacentHTML('beforeend', createMarkupCard(data))
-
-   console.log(resp);
-    return resp.json()
-});
+        const resp = await fetch(`${URL}`);
+        // if (!resp.ok) {
+        //     throw new Error(resp.statusText);
+        //   }
+        return await resp.json();
     }
-
-// async function serviceRecype() {
-//     try {
-//         const resp = await fetch(URL);
-//         const data = await resp.json()
-//         console.log(data);
-//     }
-//     catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// serviceRecype();
-
-/*=================BACK-END===========================*/
-// serviceRecype() 
-//     .then((data) => {
-// // popularRecipe.insertAdjacentElement('beforeend', createMarkupCard(data.results))
-//     })
-//     .catch(err => console.log(err))
-
-//     async function serviceRecype() {
-//         const URL =  'https://tasty-treats-backend.p.goit.global/api/recipes/popular';
-
-//         const resp = await fetch(`${URL}`);
-//         console.log(resp);
-//         return await resp.json();
-//     } 
 
 /*=================SHOW-CARD===========================*/
 
 function createMarkupCard(arr) {
-return arr.map(({preview, title, description}) => {
-    `<li class ="popular-recipe-list">
-        <img class="img-dish" src="${preview}" 
-         alt="${title}">
-        <h2 class= "name-dish">${title}</h2>
-        <h3 class="deskription-dish">${description}</h3>
-    </li>`
-}).join();
-}
+    return `${arr.map(({ id, preview, title, description, }) => {
+                return `<li key="${id}" class="popular-recipe-item">
+                          <img class="img-dish" src="${preview}" alt="${title}">
+                          <h3 class="name-dish">${title}</h2>
+                          <p class="description-dish">${description}</p>
+                        </li>`;
+              }).join('')}
+            </ul>`;
+  } 
 
-// popularRecipe.insertAdjacentHTML('beforeend',)
+console.log(popularRecipe);
