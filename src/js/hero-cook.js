2af6dcb02ed6fb450d596bf5774080ab154146c7
NPC-Roman <ispacebobo@gmail.com>
2023-09-16 17:20:48 +0300

    BugFixCook02

--- src/js/hero-cook.js
+++ src/js/hero-cook.js
@@ -7,21 +7,21 @@ fechCook().then(data => {
   const swiperCard = data
     .map(
       item => `
-      <div class="swiper-slide">
-        <div class="swiper-card">
-          <div class="cook-slide"><img src="${item.cook.imgUrl}" alt="${item.topic.name}" width="137px" height="442px"/></div>
-          <div class="plate-slide">
-            <div class="plate"><img src="${item.topic.previewUrl}" width="351px" height="468px"/></div>
-            <p class="plate-name">${item.topic.name}</p>
-            <p class="plate-sub">${item.topic.area}</p>
-          </div>
-          <div class="dish"><img src="${item.topic.previewUrl}" width="351px" height="442px"/></div>
+    <div class="swiper-slide">
+      <div class="swiper-card">
+        <div class="cook-slide"><img src="${item.cook.imgUrl}" alt="${item.topic.name}" width="137px" height="442px"/></div>
+        <div class="plate-slide">
+          <div class="plate"><img src="${item.topic.previewUrl}" width="351px" height="468px"/></div>
+          <p class="plate-name">${item.topic.name}</p>
+          <p class="plate-sub">${item.topic.area}</p>
         </div>
-      </div>`
+        <div class="dish"><img src="${item.topic.previewUrl}" width="351px" height="442px"/></div>
+      </div>
+    </div>`
     )
     .join('');
 
-  swiperContainer.insertAdjacentHTML('beforeend', swiperCard(data));
+  swiperContainer.insertAdjacentHTML('beforeend', swiperCard);
 
   /*swiper*/
   const swiper = new Swiper('.swiper', {
