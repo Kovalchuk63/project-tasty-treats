2dbce3f25fac94f3e6d9d09b5446139f1d76e467
NPC-Roman <ispacebobo@gmail.com>
2023-09-16 13:04:04 +0300

    Change03

--- src/js/hero-cook.js
+++ src/js/hero-cook.js
@@ -1,4 +1,4 @@
-import { fechCook } from '/js/api/requstCook.js';
+import { fechCook } from './js/api/requst-сook.js';
 import Swiper, { Navigation, Pagination } from 'swiper';
 import 'swiper/swiper-bundle.min.css';
 const swiperContainer = document.querySelector('.events');
@@ -9,13 +9,13 @@ fechCook().then(data => {
       item => `
       <div class="swiper-slide">
         <div class="swiper-card">
-          <div class="cook-slide"><img src="${item.cook.imgUrl}" alt="${item.topic.name}" width=137px height=442px/></div>
+          <div class="cook-slide"><img src="${item.cook.imgUrl}" alt="${item.topic.name}" width="137px" height="442px"/></div>
           <div class="plate-slide">
-            <div class="plate"><img src="${item.topic.previewUrl}" width=351px height=468px"/></div>
+            <div class="plate"><img src="${item.topic.previewUrl}" width="351px" height="468px"/></div>
             <p class="plate-name">${item.topic.name}</p>
             <p class="plate-area">${item.topic.area}</p>
           </div>
-          <div class="dish"><img src="${item.topic.previewUrl}" width=351px height=442px"/></div>
+          <div class="dish"><img src="${item.topic.previewUrl}" width="351px" height="442px"/></div>
         </div>
       </div>`
     )
@@ -27,6 +27,10 @@ fechCook().then(data => {
   const swiper = new Swiper('.swiper', {
     /*.events*/ modules: [Navigation, Pagination],
     allowSlideNext: true,
+    /*navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },*/
     pagination: {
       el: '.page' /*event*/,
       clickable: true,
