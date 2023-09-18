acce931024482aebe8737a10db73e8eba1c22829
Kovalchuk63 <vov4uk4763@gmail.com>
2023-09-18 17:41:02 +0300

    fix some js file

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
-import './js/block-categories.js';
-import './js/hero-cook.js';
-import './js/API/request-cook.js';
+
 import './js/header.js';
+import './js/hero-cook.js';
+
+import './js/block-categories.js';
+
 import './js/list-of-popular.js';
 import './js/list-of-recipes.js';
-import './js/modal-order.js';
 
 import './js/favorite-page.js'
-import './js/API/order-post.js'
+
 import './js/switcher.js';
 import './js/pagin.js'
 
 import './js/modal-recipe.js'
+import './js/modal-order.js';
+import './js/modal-rating.js';
 
 import './js/search.js';
 
+import './js/API/order-post.js'
+import './js/API/request-cook.js';
