2ff0c39f0558c020b1feea7c5eae41c41c3244da
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-09-18 13:27:51 +0300

    added heart on load

--- src/js/list-of-recipes.js
+++ src/js/list-of-recipes.js
@@ -4,6 +4,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const categoriesBtn = document.querySelector('.js-all-categories-btn');
 const cardsList = document.querySelector('.js-card-list');
 let cardsInfo = [];
+export const LS_DISHES_KEY = 'Favourite dishes';
 
 categoriesBtn.addEventListener('click', onAllCategoryButtonClick);
 
@@ -49,14 +50,21 @@ export function allCategoriesMarkup(cards) {
         </svg>`
       ).join('');
 
-      return `<li class="card-item" data-id="${_id}">
-          <svg class="card-svg-heart js-card-svg-heart" width="22px" height="22px">
+      const arrlocalStorage =
+        JSON.parse(localStorage.getItem(LS_DISHES_KEY)) ?? [];
+      if (
+        arrlocalStorage.find(
+          ({ _id: IdlocalStorage }) => IdlocalStorage === _id
+        )
+      ) {
+        return `<li class="card-item" data-id="${_id}">
+          <svg class="card-svg-heart-checked js-card-svg-heart" width="22px" height="22px">
         <use href="../sprite.svg#icon-heart"></use>
       </svg>
       <div class="image-gradient">
       <img class="card-img" src="${preview || defaults.preview}" alt="${
-        title || defaults.title
-      }"/>
+          title || defaults.title
+        }"/>
       </div>
       <div class="card-text">
       <h2 class="card-dish-name">${title || defaults.title}</h2>
@@ -71,6 +79,30 @@ export function allCategoriesMarkup(cards) {
         <button type="button" class="recipe-btn">See recipe</button>
       </div>
     </li>`;
+      } else {
+        return `<li class="card-item" data-id="${_id}">
+          <svg class="card-svg-heart js-card-svg-heart" width="22px" height="22px">
+        <use href="../sprite.svg#icon-heart"></use>
+      </svg>
+      <div class="image-gradient">
+      <img class="card-img" src="${preview || defaults.preview}" alt="${
+          title || defaults.title
+        }"/>
+      </div>
+      <div class="card-text">
+      <h2 class="card-dish-name">${title || defaults.title}</h2>
+      <p class="card-dish-descr">${description || defaults.description}</p>
+      </div>
+      <div class="rating-btn-container">
+        
+          <p class="rating-number">${rating}</p>
+          <div class="rating-container">
+          ${ratedStarsArray}${notRatedStarsArray}
+        </div>
+        <button type="button" class="recipe-btn">See recipe</button>
+      </div>
+    </li>`;
+      }
     })
     .join('');
 
@@ -115,7 +147,6 @@ export function calculationOfRatedStars(rating) {
 }
 // ==================== FAVORITES ========================
 
-export const LS_DISHES_KEY = 'Favourite dishes';
 let favouriteDishes = JSON.parse(localStorage.getItem(LS_DISHES_KEY)) ?? [];
 
 cardsList.addEventListener('click', onAddingToFavourites);
@@ -158,6 +189,3 @@ export function onAddingToFavourites(event) {
 //     }
 //   });
 // });
-
-
-if()
